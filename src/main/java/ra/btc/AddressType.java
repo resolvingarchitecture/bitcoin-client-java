696186416218eeaebfe3aa0f6ef0e918701fd008
objectorange <brian@resolvingarchitecture.io>
2021-01-12 21:36:54 -0500

    finished getnewaddress

---
+++ src/main/java/ra/btc/AddressType.java
@@ -0,0 +1,7 @@
+package ra.btc;
+
+public enum AddressType {
+    LEGACY,
+    P2SH_SEGWIT,
+    BECH32
+}
--- src/main/java/ra/btc/rpc/wallet/GetNewAddress.java
+++ src/main/java/ra/btc/rpc/wallet/GetNewAddress.java
@@ -1,11 +1,58 @@
 package ra.btc.rpc.wallet;
 
+import ra.btc.AddressType;
 import ra.btc.rpc.RPCRequest;
 
+import java.util.Map;
+
 public class GetNewAddress extends RPCRequest {
+
     public static final String NAME = "getnewaddress";
+
+    // Request
+    public String label;
+    public AddressType addressType;
+
+    // Response
+    public String address;
 
     public GetNewAddress() {
         super(NAME);
+    }
+
+    public GetNewAddress(String label) {
+        super(NAME);
+        this.label = label;
+    }
+
+    public GetNewAddress(AddressType addressType) {
+        super(NAME);
+        this.addressType = addressType;
+    }
+
+    public GetNewAddress(String label, AddressType addressType) {
+        super(NAME);
+        this.label = label;
+        this.addressType = addressType;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        // Request
+        if(label!=null) params.add(label);
+        if(addressType!=null) {
+            switch (addressType) {
+                case LEGACY: params.add("legacy");break;
+                case P2SH_SEGWIT: params.add("p2sh-segwit");break;
+                case BECH32: params.add("bech32");break;
+            }
+        }
+        return super.toMap();
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> m) {
+        // Response
+        address = (String)m.get("address");
     }
 }
