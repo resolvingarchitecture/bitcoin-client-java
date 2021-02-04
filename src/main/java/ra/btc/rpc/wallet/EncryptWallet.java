fe2e30ec64e60b6f2ee3dd578d6a01330906b1d0
objectorange <brian@resolvingarchitecture.io>
2021-02-03 21:55:31 -0500

    added encryptwallet

---
+++ src/main/java/ra/btc/rpc/wallet/EncryptWallet.java
@@ -0,0 +1,25 @@
+package ra.btc.rpc.wallet;
+
+import ra.btc.rpc.RPCRequest;
+
+import java.util.Map;
+
+public class EncryptWallet extends RPCRequest {
+
+    public static final String NAME = "encryptwallet";
+
+    // Request
+    private String passphrase;
+
+    public EncryptWallet(String passphrase) {
+        super(NAME);
+        this.passphrase = passphrase;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        // Request
+        if(passphrase!=null) params.add(passphrase);
+        return super.toMap();
+    }
+}
