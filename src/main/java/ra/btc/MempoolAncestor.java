a438cda2b3433b0ff5b1ee8512264fbad4bca3d7
objectorange <brian@resolvingarchitecture.io>
2021-03-01 10:09:33 -0500

    added getmempoolacestors

---
+++ src/main/java/ra/btc/MempoolAncestor.java
@@ -0,0 +1,26 @@
+package ra.btc;
+
+import java.util.List;
+
+public class MempoolAncestor {
+    public String transactionId;
+    public Integer size;
+    public Integer modifiedFee;
+    public Integer time;
+    public Integer height;
+    public Integer descendantCount;
+    public Integer descendantSize;
+    public Integer descendantFees;
+    public Integer ancestorCount;
+    public Integer ancestorSize;
+    public Integer ancestorFees;
+    public String wtxid;
+    public Integer feesBase;
+    public Integer feesModified;
+    public Integer feesAncestor;
+    public Integer feesDescendant;
+    public List<String> depends;
+    public List<String> spentBy;
+    public Boolean bip125Replaceable;
+
+}
---
+++ src/main/java/ra/btc/rpc/blockchain/GetMempoolAncestors.java
@@ -0,0 +1,75 @@
+package ra.btc.rpc.blockchain;
+
+import ra.btc.MempoolAncestor;
+import ra.btc.rpc.RPCRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class GetMempoolAncestors extends RPCRequest {
+    public static final String NAME = "getmempoolancestors";
+
+    public String txId;
+    public Boolean verbose = false;
+    public List<MempoolAncestor> mempoolAncestors = new ArrayList<>();
+
+    public GetMempoolAncestors(String txId) {
+        super(NAME);
+        this.txId = txId;
+    }
+
+    public GetMempoolAncestors(String txId, Boolean verbose) {
+        super(NAME);
+        this.txId = txId;
+        this.verbose = verbose;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        params.add(txId);
+        params.add(verbose);
+        return super.toMap();
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> m) {
+        if(m.size() > 0) {
+            MempoolAncestor mAnc;
+            if (verbose) {
+                List<Map<String,Object>> txs = (List<Map<String,Object>>) m.get(0);
+                for(Map<String,Object> tx : txs) {
+                    mAnc = new MempoolAncestor();
+                    mAnc.transactionId = (String)m.get("transactionId"); // TODO: Verify this is available (doesn't show in API)
+                    mAnc.size = (Integer)m.get("size");
+                    mAnc.modifiedFee = (Integer)m.get("modifiedfee");
+                    mAnc.time = (Integer)m.get("time");
+                    mAnc.height = (Integer)m.get("height");
+                    mAnc.descendantCount = (Integer)m.get("descendantcount");
+                    mAnc.descendantSize = (Integer)m.get("descendantsize");
+                    mAnc.descendantFees = (Integer)m.get("descendantfees");
+                    mAnc.ancestorCount = (Integer)m.get("ancestorcount");
+                    mAnc.ancestorSize = (Integer)m.get("ancestorsize");
+                    mAnc.ancestorFees = (Integer)m.get("ancestorfees");
+                    mAnc.wtxid = (String)m.get("wtxid");
+                    Map<String,Object> fees = (Map<String,Object>)m.get("fees");
+                    mAnc.feesBase = (Integer)fees.get("base");
+                    mAnc.feesModified = (Integer)fees.get("modified");
+                    mAnc.feesAncestor = (Integer)fees.get("ancestor");
+                    mAnc.feesDescendant = (Integer)fees.get("descendant");
+                    mAnc.depends = (List<String>)m.get("depends");
+                    mAnc.spentBy = (List<String>)m.get("spendby");
+                    mAnc.bip125Replaceable = (Boolean)m.get("bip125-replaceable");
+                    mempoolAncestors.add(mAnc);
+                }
+            } else {
+                List<String> txIds = (List<String>) m.get(0);
+                for (String txId : txIds) {
+                    mAnc = new MempoolAncestor();
+                    mAnc.transactionId = txId;
+                    mempoolAncestors.add(mAnc);
+                }
+            }
+        }
+    }
+}
