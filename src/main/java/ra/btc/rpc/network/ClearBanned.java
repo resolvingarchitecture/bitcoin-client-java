a28ae986923b102aded9a39f6b404168a58f195b
objectorange <brian@resolvingarchitecture.io>
2021-03-19 09:37:31 -0400

    added clearbanned

---
+++ src/main/java/ra/btc/rpc/network/ClearBanned.java
@@ -0,0 +1,16 @@
+package ra.btc.rpc.network;
+
+import ra.btc.rpc.RPCRequest;
+
+/**
+ * Clear all banned IPs.
+ */
+public class ClearBanned extends RPCRequest {
+
+    public static final String NAME = "clearbanned";
+
+    public ClearBanned() {
+        super(NAME);
+    }
+
+}
