7d756bf863b6d5729de5ae20d1aee66c05643064
objectorange <brian@resolvingarchitecture.io>
2021-04-05 10:17:17 -0400

    added DeriveAddress

---
+++ src/main/java/ra/btc/rpc/util/DeriveAddress.java
@@ -0,0 +1,60 @@
+package ra.btc.rpc.util;
+
+import ra.btc.rpc.RPCRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Creates a multi-signature address with n signature of m keys required.
+ *
+ * It returns a json object with the address and redeemScript.
+ */
+public class DeriveAddress extends RPCRequest {
+    public static final String NAME = "deriveaddresses";
+
+    // Request
+    public final String descriptor;
+    public final Integer begin;
+    public final Integer end;
+
+    // Response
+    public List<String> addresses;
+
+    public DeriveAddress(String descriptor) {
+        super(NAME);
+        this.descriptor = descriptor;
+        this.begin = null;
+        this.end = null;
+    }
+
+    public DeriveAddress(String descriptor, Integer begin, Integer end) {
+        super(NAME);
+        this.descriptor = descriptor;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        params.add(descriptor);
+        if(end!=null) {
+            if(begin!=null)
+                params.add(Arrays.asList(begin,end));
+            else
+                params.add(end);
+        }
+        return super.toMap();
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> m) {
+        if(m.size() > 0) {
+            for(Object obj : m.values()) {
+                addresses = (List<String>)obj;
+                break;
+            }
+        }
+    }
+}
