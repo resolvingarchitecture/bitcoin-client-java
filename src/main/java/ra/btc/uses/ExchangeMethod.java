bd2e4f38acdc529614f5f6e747a638c7673177a8
objectorange <brian@resolvingarchitecture.io>
2021-08-06 11:18:16 -0400

    added exchange methods

---
+++ src/main/java/ra/btc/uses/ExchangeMethod.java
@@ -0,0 +1,11 @@
+package ra.btc.uses;
+
+public enum ExchangeMethod {
+    F2F,
+    InteracETransfer,
+    SEPA_FI, // Finland
+    SEPA_PT, // Portugal
+    SEPA_SE, // Sweden
+    SEPA_SK, // Slovakia
+    Zelle
+}
