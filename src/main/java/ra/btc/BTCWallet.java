e7996e306c1a4fac1fed17d15e409abe39527ef0
objectorange <1m5@protonmail.com>
2021-12-06 09:27:58 -0500

    moved crypto wallet from common into bitcoin client

--- src/main/java/ra/btc/BTCWallet.java
+++ src/main/java/ra/btc/BTCWallet.java
@@ -1,16 +1,21 @@
 package ra.btc;
 
+import ra.common.JSONParser;
+import ra.common.JSONPretty;
+import ra.common.JSONSerializable;
 import ra.common.currency.crypto.BTC;
-import ra.common.currency.crypto.Crypto;
-import ra.common.currency.wallet.CryptoWallet;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Objects.nonNull;
 
-public class BTCWallet extends CryptoWallet {
+public class BTCWallet implements JSONSerializable {
 
+    private String name;
+    private Integer version;
+    private BigInteger balance;
     private String format; // database format - bdb or sqlite
     private Integer keypoololdest; // the timestamp (seconds since Unix epoch) of the oldest pre-generated key in the key pool
     private Integer keypoolsize; // how many new keys are pre-generated (only counts external keys)
@@ -19,6 +24,33 @@ public class BTCWallet extends CryptoWallet {
     private Integer paytxfee; // the transaction fee configuration, set in Sats/kB
     private String hdseedid; // the Hash160 of the HD seed (only present when HD is enabled)
     private Boolean privateKeysEnabled; // false if privatekeys are disabled for this wallet (enforced watch-only wallet)
+    private BigInteger unconfirmedBalance; // the total unconfirmed balance of the wallet in Sats
+    private BigInteger immatureBalance; // the total immature balance of the wallet in Sats
+    private BigInteger txCount; // the total number of transactions in the wallet in Sats
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public BigInteger getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigInteger balance) {
+        this.balance = balance;
+    }
 
     public String getFormat() {
         return format;
@@ -84,9 +116,36 @@ public class BTCWallet extends CryptoWallet {
         this.privateKeysEnabled = privateKeysEnabled;
     }
 
+    public BigInteger getUnconfirmedBalance() {
+        return unconfirmedBalance;
+    }
+
+    public void setUnconfirmedBalance(BigInteger unconfirmedBalance) {
+        this.unconfirmedBalance = unconfirmedBalance;
+    }
+
+    public BigInteger getImmatureBalance() {
+        return immatureBalance;
+    }
+
+    public void setImmatureBalance(BigInteger immatureBalance) {
+        this.immatureBalance = immatureBalance;
+    }
+
+    public BigInteger getTxCount() {
+        return txCount;
+    }
+
+    public void setTxCount(BigInteger txCount) {
+        this.txCount = txCount;
+    }
+
     @Override
     public Map<String, Object> toMap() {
-        Map<String,Object> m = super.toMap();
+        Map<String,Object> m = new HashMap<>();
+        m.put("walletname", name);
+        m.put("walletversion", version);
+        m.put("balance", balance);
         m.put("format", format);
         m.put("keypoololdest", keypoololdest);
         m.put("keypoolsize", keypoolsize);
@@ -100,7 +159,9 @@ public class BTCWallet extends CryptoWallet {
 
     @Override
     public void fromMap(Map<String, Object> m) {
-        super.fromMap(m);
+        if(nonNull(m.get("walletname"))) name = (String)m.get("walletname");
+        if(nonNull(m.get("walletversion"))) version = (Integer)m.get("walletversion");
+        if(nonNull(m.get("balance"))) balance = (BigInteger)m.get("balance");
         if(m.get("format")!=null) format = (String)m.get("format");
         if(m.get("keypoololdest")!=null) keypoololdest = (Integer)m.get("keypoololdest");
         if(m.get("keypoolsize")!=null) keypoolsize = (Integer)m.get("keypoolsize");
@@ -110,7 +171,19 @@ public class BTCWallet extends CryptoWallet {
         if(m.get("hdseedid")!=null) hdseedid = (String)m.get("hdseedid");
         if(m.get("private_keys_enabled")!=null) privateKeysEnabled = (Boolean)m.get("private_keys_enabled");
         if(nonNull(m.get("unconfirmed_balance"))) unconfirmedBalance = new BTC((Double)m.get("unconfirmed_balance")).value();
+        if(nonNull(m.get("unconfirmedBalance"))) unconfirmedBalance = (BigInteger)m.get("unconfirmedBalance");
         if(nonNull(m.get("immature_balance"))) immatureBalance = new BTC((Double)m.get("immature_balance")).value();
+        if(nonNull(m.get("immatureBalance"))) immatureBalance = (BigInteger)m.get("immatureBalance");
+        if(nonNull(m.get("txcount"))) txCount = (BigInteger)m.get("txcount");
     }
 
+    @Override
+    public String toJSON() {
+        return JSONPretty.toPretty(JSONParser.toString(toMap()), 4);
+    }
+
+    @Override
+    public void fromJSON(String json) {
+        fromMap((Map<String,Object>)JSONParser.parse(json));
+    }
 }
